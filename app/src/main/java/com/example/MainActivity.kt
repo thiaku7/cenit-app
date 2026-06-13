ae44ac68847e7287f391725343730d1952a9dd6b
thiaku7 <thiagoriosaranda18@gmail.com>
2026-06-13 16:48:13 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -14,10 +14,12 @@ import com.example.ui.screens.LoginScreen
 import com.example.ui.screens.MainScreen
 import com.example.ui.screens.WelcomeScreen
 import com.example.ui.theme.MyApplicationTheme
+import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
         enableEdgeToEdge()
         setContent {
             val viewModel: CenitViewModel = viewModel()
