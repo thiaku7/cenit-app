eeed50b39833d73a7ffa7c60a5c70abc41bcd2cb
thiaku7 <thiagoriosaranda18@gmail.com>
2026-06-13 15:18:02 -0300

    Update CenitApplication.kt

--- .github/workflows/CenitApplication.kt
+++ .github/workflows/CenitApplication.kt
@@ -1,11 +1,1 @@
-package com.example
 
-import android.app.Application
-import com.google.firebase.FirebaseApp
-
-class CenitApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        FirebaseApp.initializeApp(this)
-    }
-}
